3d418ecc0e296cf4f6333697c856db522dc4e078
RISHII7 <rishikesh.palande07@gmail.com>
2024-11-19 00:42:18 +0530

    Integrations Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "@/sections/Hero";
 import Navbar from "@/sections/Navbar";
 import Features from "@/sections/Features";
 import LogoTicker from "@/sections/LogoTicker";
+import Integrations from "@/sections/Integrations";
 import Introduction from "@/sections/Introduction";
 
 export default function Home() {
@@ -12,6 +13,7 @@ export default function Home() {
             <LogoTicker />
             <Introduction />
             <Features />
+            <Integrations />
         </>
     );
 }
