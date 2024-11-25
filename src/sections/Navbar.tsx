58b653fd7aded4ddfab53577661ed60d3bbfcffa
RISHII7 <rishikesh.palande07@gmail.com>
2024-11-25 21:10:30 +0530

    Hamburger Icon Animation

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,7 +1,11 @@
+"use client"
+
 import Image from "next/image";
+import { useState } from "react";
 
 import Button from "@/components/Button";
 import logoImage from "@/assets/images/logo.svg"
+import { twMerge } from "tailwind-merge";
 
 const navLinks = [
     { label: "Home", href: "#" },
@@ -11,16 +15,19 @@ const navLinks = [
 ];
 
 export default function Navbar() {
+
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
-        <section className="py-4 lg:py-8">
+        <section className="py-4 lg:py-8 sticky top-0 z-50">
             <div className="container max-w-5xl">
-                <div className="grid grid-cols-2 lg:grid-cols-3 border border-white/15 rounded-full p-2 px-4 md:pr-2 items-center">
+                <div className="grid grid-cols-2 lg:grid-cols-3 border border-white/15 rounded-full p-2 px-4 md:pr-2 items-center bg-neutral-950/70 backdrop-blur">
                     <div>
-                        <Image 
-                            src={logoImage} 
-                            alt="Layers Logo" 
+                        <Image
+                            src={logoImage}
+                            alt="Layers Logo"
                             className="h-9 md:h-auto w-auto"
-                        /> 
+                        />
                     </div>
 
                     <div className="lg:flex justify-center items-center hidden">
@@ -32,23 +39,47 @@ export default function Navbar() {
                             ))}
                         </nav>
                     </div>
-                    
+
                     <div className="flex justify-end gap-4">
-                        <svg 
-                            xmlns="http://www.w3.org/2000/svg" 
-                            width="24" 
-                            height="24" 
-                            viewBox="0 0 24 24" 
-                            fill="none" 
-                            stroke="currentColor" 
-                            stroke-width="2" 
-                            stroke-linecap="round" 
-                            stroke-linejoin="round" 
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
                             className="feather feather-menu md:hidden"
+                            onClick={() => setIsOpen(!isOpen)}
                         >
-                            <line x1="3" y1="12" x2="21" y2="12"></line>
-                            <line x1="3" y1="6" x2="21" y2="6"></line>
-                            <line x1="3" y1="18" x2="21" y2="18"></line>
+                            <line
+                                x1="3"
+                                y1="6"
+                                x2="21"
+                                y2="6"
+                                className={twMerge(
+                                    "origin-left transition",
+                                    isOpen && "rotate-45 -translate-y-1"
+                                )}></line>
+                            <line
+                                x1="3"
+                                y1="12"
+                                x2="21" y2="12"
+                                className={twMerge(
+                                    "transition",
+                                    isOpen && "opacity-0"
+                                )}></line>
+                            <line
+                                x1="3"
+                                y1="18"
+                                x2="21"
+                                y2="18"
+                                className={twMerge(
+                                    "origin-left transition",
+                                    isOpen && "-rotate-45 translate-y-1"
+                                )}></line>
                         </svg>
                         <Button variant="secondary" className="hidden md:inline-flex items-center">Log In</Button>
                         <Button variant="primary" className="hidden md:inline-flex items-center">Sign Up</Button>
